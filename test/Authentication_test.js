090ac4dfed6ff93c7246704f9db1161618865655
John Paulo Pamintuan <jpamintuan@apple.com>
2020-07-05 22:27:01 +0800

    Updated Authentication_test.js to use json data

--- test/Authentication_test.js
+++ test/Authentication_test.js
@@ -1,43 +1,31 @@
 const expect = require('chai').expect;
 const {I} = inject();
 
+var auth_data = require('../data/auth.json');
+
 Feature('Authentication tests');
 
-let valid_login_creds = {
-	email: 'eve.holt@reqres.in',
-	password: 'cityslicka'							  
-}
 Scenario('Verify successful login', async () => {
-	const res = await I.sendPostRequest('/api/login', valid_login_creds);
-	expect(res.status).to.eql(200);
-	expect(res.data.token).to.eql("QpwL5tke4Pnpja7X4");
+	const res = await I.sendPostRequest(auth_data.Login.url, auth_data.Login.successful.credentials);
+	expect(res.status).to.eql(auth_data.Login.successful.status);
+	expect(res.data.token).to.eql(auth_data.Login.successful.token);
 });
 
-let invalid_login_creds = {
-	email: 'peter@klaven'						  
-}
 Scenario('Verify unsuccessful login', async () => {
-	const res = await I.sendPostRequest('/api/login', invalid_login_creds);
-	expect(res.status).to.eql(400);
-	expect(res.data.error).to.eql("Missing password");
+	const res = await I.sendPostRequest(auth_data.Login.url, auth_data.Login.unsuccessful.credentials);
+	expect(res.status).to.eql(auth_data.Login.unsuccessful.status);
+	expect(res.data.error).to.eql(auth_data.Login.unsuccessful.error);
 });
 
-let valid_registration_creds = {
-	email: 'eve.holt@reqres.in',
-	password: 'pistol'						  
-}
 Scenario('Verify successful registration', async () => {
-	const res = await I.sendPostRequest('/api/register', valid_registration_creds);
-	expect(res.status).to.eql(200);
-	expect(res.data.id).to.eql(4);
-	expect(res.data.token).to.eql("QpwL5tke4Pnpja7X4");
+	const res = await I.sendPostRequest(auth_data.Registration.url, auth_data.Registration.successful.credentials);
+	expect(res.status).to.eql(auth_data.Registration.successful.status);
+	expect(res.data.id).to.eql(auth_data.Registration.successful.id);
+	expect(res.data.token).to.eql(auth_data.Registration.successful.token);
 });
 
-let invalid_registration_creds = {
-	"email": "sydney@fife"						  
-}
 Scenario('Verify unsuccessful registration', async () => {
-	const res = await I.sendPostRequest('/api/register', invalid_registration_creds);
-	expect(res.status).to.eql(400);
-	expect(res.data.error).to.eql("Missing password");
+	const res = await I.sendPostRequest(auth_data.Registration.url, auth_data.Registration.unsuccessful.credentials);
+	expect(res.status).to.eql(auth_data.Registration.unsuccessful.status);
+	expect(res.data.error).to.eql(auth_data.Registration.unsuccessful.error);
 });
